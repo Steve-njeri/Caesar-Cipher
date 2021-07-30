019b2a407dd9f602644285a68233e6c33754a5f9
Steve-njeri <stevenjeri254@gmail.com>
2021-07-30 21:59:55 +0300

    modified gradle , Encoding.java , EncodingTest.java and added a new file App.java

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,2 +1,8 @@
 public class Encoding {
+    private int dKey;
+    private String dInputText;
+    public Encoding(int key, String inputText) {
+        dKey = key;
+        dInputText = inputText;
+    }
 }
